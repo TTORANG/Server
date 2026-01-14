a70068361319b0f2d4b8730ea624f1793c0571e4
김도연 <kimdoyeon0113@naver.com>
2026-01-15 01:32:24 +0900

    chore: swagger 기본 셋팅

---
+++ src/swagger.config.js
@@ -0,0 +1,35 @@
+// src/swagger.config.js
+import swaggerJSDoc from "swagger-jsdoc";
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "또랑 서비스 API",
+      version: "1.0.0",
+      description: "또랑의 서버 API 명세서입니다.",
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+        description: "로컬 테스트 서버",
+      },
+      {
+        url: "https://ttorang-server-.asia-northeast3.run.app",
+        description: "개발용 배포 서버 (GCP)",
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+    },
+  },
+  apis: ["./src/controllers/*.js", "./src/index.js"],
+};
+
+export const specs = swaggerJSDoc(options);
